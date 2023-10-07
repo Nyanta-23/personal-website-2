3a7c6d67027d2bd986d1e65781d8cb888a52c942
Nyanta-23 <muhamadilhan02404@gmail.com>
2023-10-07 11:47:00 +0700

    resolving and adding some position and script for interaction

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,15 +1,29 @@
 "use strict"
 
+const navbar = document.getElementById('navbar');
+
 const navMob = document.getElementById('mobile');
 const buttonMob = document.getElementById('btn-mob');
 const lineAnim = buttonMob.getElementsByTagName('span');
 
+
+// Left Style Responsive Mobile Adjustment
+navMob.style.left = `${window.innerWidth}px`;
+
+
 // Button Mobile Navigation
 buttonMob.addEventListener('click', () => {
-  navMob.classList.toggle('display-mobile');
+  navMob.classList.toggle('show-mobile');
 
   for (let i = 0; i < lineAnim.length; i++) {
     lineAnim[i].classList.toggle('anim-direct')
   }
 });
 
+
+// Scroll effect
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 10) {
+    navbar.classList.toggle('');
+  }
+});
